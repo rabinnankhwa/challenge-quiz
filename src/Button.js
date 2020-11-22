45f20ee199135212f7a12024b5abc1434cb99c47
Rabin Nankhwa <rabin.nankhwa@gmail.com>
2020-11-22 13:00:49 +0545

    feat: highlight clicked button fulfilling incorrect button case

--- src/Button.js
+++ src/Button.js
@@ -3,15 +3,34 @@ import React from 'react'
 import './Button.css'
 
 class Button extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      clicked: false
+    }
+  }
+
+  handleButtonClick () {
+    this.setState({ clicked: true })
+    this.props.handleAnswerClick(this.props.value)
+  }
+
+  componentDidUpdate (prevProps) {
+    if (prevProps.value.data !== this.props.value.data) {
+      this.setState({ clicked: false })
+    }
+  }
+
   render () {
-    const { value, completed, handleAnswerClick } = this.props
+    const { value, completed } = this.props
+    const { clicked } = this.state
     const buttonText = decodeURIComponent(value.data)
     return (
       <div className='button-wrapper'>
         <button
           disabled={completed}
-          className='button'
-          onClick={() => handleAnswerClick(value)}
+          className={`button ${clicked ? 'button-incorrect' : ''}`}
+          onClick={() => this.handleButtonClick(value)}
         >
           {buttonText}
         </button>
